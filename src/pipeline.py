9bdb09b1ae1e5f4f107a25ff54e8569a0125cca6
SylvainJ66 <sylvain.jouglard.66@gmail.com>
2023-02-05 12:02:12 +0100

    reload

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,11 +1,3 @@
-NO_TESTS = "No tests"
-TESTS_PASSED = "Tests passed"
-DEPLOYMENT_FAILED = "Deployment failed"
-COMPLETED_SUCCESSFULLY = "Deployment completed successfully"
-DEPLOYMENT_SUCCESSFUL = "Deployment successful"
-LOG_FAILED = "Tests failed"
-
-
 class Pipeline:
     def __init__(self, config, emailer, log):
         self.config = config
@@ -13,26 +5,38 @@ class Pipeline:
         self.log = log
 
     def run(self, project):
+        tests_passed = False
+        deploy_successful = False
+
         if project.has_tests():
-            if "success" != project.run_tests():
-                self.log.error(LOG_FAILED)
-                self.send_email(LOG_FAILED)
-                return
-
-            self.log.info(TESTS_PASSED)
+            if "success" == project.run_tests():
+                self.log.info("Tests passed")
+                tests_passed = True
+            else:
+                self.log.error("Tests failed")
+                tests_passed = False
         else:
-            self.log.info(NO_TESTS)
+            self.log.info("No tests")
+            tests_passed = True
 
-        if "success" == project.deploy():
-            self.log.info(DEPLOYMENT_SUCCESSFUL)
-            self.send_email(COMPLETED_SUCCESSFULLY)
+        if tests_passed:
+            if "success" == project.deploy():
+                self.log.info("Deployment successful")
+                deploy_successful = True
+            else:
+                self.log.error("Deployment failed")
+                deploy_successful = False
         else:
-            self.log.error(DEPLOYMENT_FAILED)
-            self.send_email(DEPLOYMENT_FAILED)
+            deploy_successful = False
 
-    def send_email(self, email_message):
         if self.config.send_email_summary():
             self.log.info("Sending email")
-            self.emailer.send(email_message)
+            if tests_passed:
+                if deploy_successful:
+                    self.emailer.send("Deployment completed successfully")
+                else:
+                    self.emailer.send("Deployment failed")
+            else:
+                self.emailer.send("Tests failed")
         else:
             self.log.info("Email disabled")
